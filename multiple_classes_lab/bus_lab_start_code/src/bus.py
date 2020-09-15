2d1a6c145791749609cd5dc5cb7baef5160f0ef9
Douglas MacIntyre <dougmac21@talk21.com>
2020-09-15 15:00:00 +0100

    wrote the Bus class

---
+++ multiple_classes_lab/bus_lab_start_code/src/bus.py
@@ -0,0 +1,8 @@
+class Bus:
+    def __init__(self, route_number, destination):
+        self.route_number = route_number
+        self.destination = destination
+
+    def noise_made_by_bus():
+        return "Brum brum"
+
