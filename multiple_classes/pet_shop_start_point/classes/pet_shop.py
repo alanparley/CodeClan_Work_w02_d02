0d866d9518e809c57c60b94d0fceecc140e87c66
Douglas MacIntyre <dougmac21@talk21.com>
2020-09-15 14:10:11 +0100

    solved problem 9 just ahead of zsolt explanation. issue was inconsistency in the referred object name.

--- multiple_classes/pet_shop_start_point/classes/pet_shop.py
+++ multiple_classes/pet_shop_start_point/classes/pet_shop.py
@@ -6,24 +6,33 @@ class PetShop:
         self.total_cash = cash_in_till              # the self.<name> values (the property names) MUST match up to the names which are expected in the assertequal part of the tests. on the right hand side names can be internally changed (within the class or the method) to the users specifications.
         self.pets_sold = 0
 
-
+#4
     def stock_count(self,):
         return len(self.pets)
-    
+#5    
     def increase_pets_sold(self,):
         self.pets_sold += 1
-
+#6
     def increase_total_cash(self, cash_transaction):
         self.total_cash += cash_transaction
-
+#7
     def add_pet(self, pet_to_add):
         self.pets.append(pet_to_add)
-
+#8
     def remove_pet(self, pet_to_remove):
         self.pets.remove(pet_to_remove)
-
+#9
     def find_pet_by_name(self, name_of_pet):
-        for each_pet in self.pets:
-            if self.pet.name == name_of_pet:
-                return self.pet
+        for any_pet in self.pets:
+            if any_pet.name == name_of_pet:
+                return any_pet
+
+
+
+
+# do you need to use a return?
+# if  functions job is to bring back data, its a return
+# if the job o the function is to chane something, we dont need a return
+# if you GET something, youll almost certianlyhave to RETURN something
+# eg increase pets sold isnt a get, so its a change to something and no need to return - practice makes perfect.
 
--- multiple_classes/pet_shop_start_point/tests/pet_shop_test.py
+++ multiple_classes/pet_shop_start_point/tests/pet_shop_test.py
@@ -24,6 +24,7 @@ class TestPetShop(unittest.TestCase):
         self.assertEqual(0, self.pet_shop.pets_sold)
 #4
 #stock_count is a method with no arguments passed - it is checking that the number of entries in the list of pets is 2.
+# additionally the name of the function indicates that it is counting something!
     def test_pet_shop_stock_count(self):
         self.assertEqual(2, self.pet_shop.stock_count())
 #5
@@ -37,7 +38,8 @@ class TestPetShop(unittest.TestCase):
         self.pet_shop.increase_total_cash(500)
         self.assertEqual(1500, self.pet_shop.total_cash)
 #7
-
+# you dont always add everything to the defsetup because sometimes the data that is being used is only used for one or few tests. Thats why we dont need to have this additional pet in the defsetup list of pets
+# this is particularly important when it comes to large or complex testing suites because the larger the startup data structure the longer that the testing will take to run (Zsolt mentioned that cutting down on the defsetup data saved 14 minutes on a 16 minute test at criton.)
     def test_can_add_pet_to_stock(self):
         new_pet = Pet("Lancelot", "dog", "Basset Hound", 750)
         self.pet_shop.add_pet(new_pet)
@@ -48,10 +50,10 @@ class TestPetShop(unittest.TestCase):
         self.pet_shop.remove_pet(self.pet_1)
         self.assertEqual(1, self.pet_shop.stock_count())
 #9
-
+# this is back to nested objects so tricker than the revious ones
     def test_can_find_pet_by_name(self):
         pet = self.pet_shop.find_pet_by_name("Sir Percy")
-        self.assertEqual("Sir Percy", pet.name)
+        self.assertEqual("Sir Percy", pet.name)    # need to return the entire pet object so that the .name can be run on it in the check
 # work until here
 #10
     @unittest.skip("delete this line to run the test")
